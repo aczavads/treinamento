a50bb7e8c2c9869341b0cb664026205228b79cbb
thaisacs <thaiscamachoo@gmail.com>
2020-01-08 17:30:01 -0200

    refatoração

--- src/main/java/thais/matera/dia3/factory/ConsoleLogger.java
+++ src/main/java/thais/matera/dia3/factory/ConsoleLogger.java
@@ -3,6 +3,8 @@ package thais.matera.dia3.factory;
 import javax.swing.JOptionPane;
 
 public class ConsoleLogger implements Logger {
+	
+	@Override
 	public void log(String message) {
 		System.out.println(message);
 	}
--- src/main/java/thais/matera/dia3/factory/GraphicLogger.java
+++ src/main/java/thais/matera/dia3/factory/GraphicLogger.java
@@ -3,6 +3,8 @@ package thais.matera.dia3.factory;
 import javax.swing.JOptionPane;
 
 public class GraphicLogger implements Logger {
+	
+	@Override
 	public void log(String message) {
 		JOptionPane.showMessageDialog(null, message);
 	}

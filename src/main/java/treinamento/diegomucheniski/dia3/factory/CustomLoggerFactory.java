56e7569111e3cbaeeea438c25812b323df69df1f
diego.mucheniski <diego.mucheniski@matera.com>
2020-01-09 09:20:38 -0300

    Ajuste no nome do parametro passado para o Factory

--- src/main/java/treinamento/diegomucheniski/dia3/factory/CustomLoggerFactory.java
+++ src/main/java/treinamento/diegomucheniski/dia3/factory/CustomLoggerFactory.java
@@ -4,19 +4,19 @@ import treinamento.diegomucheniski.dia4.composer.CustomLoggerComsoleComGrafico;
 
 public abstract class CustomLoggerFactory {
 
-	public static CustomLogger createInstance(String metodo) {
+	public static CustomLogger createInstance(String tipo) {
 
-		if (metodo.equals("console")) {
+		if (tipo.equals("console")) {
 			return new CustomLoggerConsole();
 		}
-		if (metodo.equals("grafico")) {
+		if (tipo.equals("grafico")) {
 			return new CustomLoggerGrafico();
 		}
-		if (metodo.equals("console-grafico")) {
+		if (tipo.equals("console-grafico")) {
 			return new CustomLoggerComsoleComGrafico();
 		}
 		
-		if (metodo.equals("console-triplo")) {
+		if (tipo.equals("console-triplo")) {
 			CustomLoggerComposite composite = new CustomLoggerComposite();
 			composite.addLogger(new CustomLoggerConsole());
 			composite.addLogger(new CustomLoggerConsole());

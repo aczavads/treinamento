bc5e05059a879da1502b2fd7a30cb022a5e1d292
Guilherme Beidaki Peres <guilherme.peres@matera.com>
2020-01-09 09:37:20 -0300

    implementação de composite e observer

--- src/main/java/treinamento/guilhermeperes/dia4/composite/App.java
+++ src/main/java/treinamento/guilhermeperes/dia4/composite/App.java
@@ -1,11 +1,15 @@
 package treinamento.guilhermeperes.dia4.composite;
 
+import treinamento.guilhermeperes.dia3.patterns.factory.ConsoleLogger;
 import treinamento.guilhermeperes.dia3.patterns.factory.Logger;
 import treinamento.guilhermeperes.dia3.patterns.factory.LoggerFactory;
 
 public class App {
 	public static void main(String[] args) {
-		Logger graficoConsoleLogger = LoggerFactory.createLogger(LoggerComConsoleEGrafico.class);
+		Logger graficoConsoleLogger = new LoggerComposite(
+				LoggerFactory.createLogger(ConsoleLogger.class),
+				LoggerFactory.createLogger(GraficoLogger.class)
+			);
 		
 		graficoConsoleLogger.log("Guilherme");
 	}
---
+++ src/main/java/treinamento/guilhermeperes/dia4/composite/NullLogger.java
@@ -0,0 +1,10 @@
+package treinamento.guilhermeperes.dia4.composite;
+
+import treinamento.guilhermeperes.dia3.patterns.factory.Logger;
+
+public class NullLogger implements Logger {
+
+	@Override
+	public void log(String text) {
+	}
+}

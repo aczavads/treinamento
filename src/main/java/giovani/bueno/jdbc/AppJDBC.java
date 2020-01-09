9f51a9ed349a6bcedad093a2519b1287d59f816a
gbueno1291 <giovani_bueno@live.com>
2020-01-09 14:54:12 -0200

    Comit repository com JDBC

--- src/main/java/giovani/bueno/jdbc/AppJDBC.java
+++ src/main/java/giovani/bueno/jdbc/AppJDBC.java
@@ -1,15 +1,13 @@
 package giovani.bueno.jdbc;
 
+
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.UUID;
 
-import javax.swing.JOptionPane;
+import giovani.bueno.jdbc.repository.PessoaRepository;
 
 
 public class AppJDBC {
@@ -20,83 +18,101 @@ public class AppJDBC {
 	
 	public static void main(String[] args) {
 		try {
+			
 			Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
-			createTablePessoa(conn);
-			conn.setAutoCommit(false);
+		   PessoaRepository pessoaRepository = new PessoaRepository(conn);
+		   conn.setAutoCommit(false);
+
+//			pessoaRepository.createTable();
+//			JOptionPane.showMessageDialog(null, "Transação em andamento..");
+//			pessoaRepository.insert(giovani);
+//			pessoaRepository.insert(laura);
+//			pessoaRepository.insert(rafael);
+//			
+//			giovani.setNome("Giovani Bueno");
+//			pessoaRepository.update(giovani);
+//			
+//			pessoaRepository.delete(rafael);
+//			
+//			List<Pessoa> pessoas =   pessoaRepository.selectAll();
+//			pessoas.forEach(p -> System.out.println(p));
 			
-			insertPessoa(conn, giovani);
-			insertPessoa(conn, laura);
-			insertPessoa(conn, rafael);
-			JOptionPane.showMessageDialog(null, "Transação em andamento..");			
+			long inicio  = System.currentTimeMillis();
 			
-			giovani.setNome("Giovani Bueno");
-			updatePessoa(conn,giovani);
-			
-			deletePessoa(conn, rafael);
+			for (int i = 0; i < 1000; i++) {
+				UUID uuid = UUID.randomUUID();
+				String myRandom = uuid.toString();
+				System.out.println(myRandom.substring(0,20));
+			    Pessoa p = new Pessoa(UUID.randomUUID(),i+4,myRandom);
+			    pessoaRepository.insert(p);
+			}
 			conn.commit();
-			
-			List<Pessoa> pessoas =   recuperarTodas(conn);
-			pessoas.forEach(p -> System.out.println(p));
+			System.out.println("Tempo total: " +(System.currentTimeMillis()-inicio));
 			
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 		System.out.println("FIM");
 	}
-
-	private static List<Pessoa> recuperarTodas(Connection conn) throws SQLException {
-		List<Pessoa> listPessoas = new ArrayList<>();
-		ResultSet rsPessoas = conn.createStatement().executeQuery("select id, codigo,nome from pessoa");
-		while(rsPessoas.next()) {
-			Pessoa pessoaRecuperada = new Pessoa(		
-		        (UUID) rsPessoas.getObject("id"),
-		        rsPessoas.getInt("codigo"),
-		        rsPessoas.getString("nome"));
-		  listPessoas.add(pessoaRecuperada);
-		}
-		return listPessoas;
-		
-	}
-
-	private static void insertPessoa(Connection conn,Pessoa pessoa) throws SQLException {
-		PreparedStatement psInsert = conn.prepareStatement(
-				"insert into pessoa(id,codigo,nome) values(?,?,?)"); 
-		
-		psInsert.setObject(1, pessoa.getId());
-		psInsert.setInt(2, pessoa.getCodigo());
-		psInsert.setString(3,pessoa.getNome());
-		psInsert.executeUpdate();
-	}
-
-	private static void updatePessoa(Connection conn, Pessoa pessoa) throws SQLException {
-		PreparedStatement psUpdate = conn.prepareStatement(
-				"update pessoa set nome=?, codigo = ? where id=?");
-		psUpdate.setString(1,pessoa.getNome());
-		psUpdate.setInt(2, pessoa.getCodigo());
-		psUpdate.setObject(3, pessoa.getId());
-		
-		psUpdate.executeUpdate();
-	}
 	
-	private static void deletePessoa(Connection conn, UUID id) throws SQLException {
-		PreparedStatement psDelete = conn.prepareStatement("Delete from pessoa where id = ?");
-		psDelete.setObject(1,id);
-		psDelete.executeUpdate();	
-	}
-
-	private static void deletePessoa(Connection conn, Pessoa pessoa) throws SQLException {
-	  deletePessoa(conn, pessoa.getId());
-	}
 	
-
-	private static void createTablePessoa(Connection conn) throws SQLException {
-      conn.createStatement().execute(
-    		  "create table if not exists pessoa ("
-    		  + "id uuid not null primary key,"
-    		  + "codigo integer not null unique,"
-    		  + "nome varchar(255) not null"
-    		  + ")");
-      conn.createStatement().execute("delete from pessoa");
+	public static void testeDePreparedStatement() {
+		
 	}
+
+//	private static List<Pessoa> recuperarTodas(Connection conn) throws SQLException {
+//		List<Pessoa> listPessoas = new ArrayList<>();
+//		ResultSet rsPessoas = conn.createStatement().executeQuery("select id, codigo,nome from pessoa");
+//		while(rsPessoas.next()) {
+//			Pessoa pessoaRecuperada = new Pessoa(		
+//		        (UUID) rsPessoas.getObject("id"),
+//		        rsPessoas.getInt("codigo"),
+//		        rsPessoas.getString("nome"));
+//		  listPessoas.add(pessoaRecuperada);
+//		}
+//		return listPessoas;
+//		
+//	}
+//
+//	private static void insertPessoa(Connection conn,Pessoa pessoa) throws SQLException {
+//		PreparedStatement psInsert = conn.prepareStatement(
+//				"insert into pessoa(id,codigo,nome) values(?,?,?)"); 
+//		
+//		psInsert.setObject(1, pessoa.getId());
+//		psInsert.setInt(2, pessoa.getCodigo());
+//		psInsert.setString(3,pessoa.getNome());
+//		psInsert.executeUpdate();
+//	}
+//
+//	private static void updatePessoa(Connection conn, Pessoa pessoa) throws SQLException {
+//		PreparedStatement psUpdate = conn.prepareStatement(
+//				"update pessoa set nome=?, codigo = ? where id=?");
+//		psUpdate.setString(1,pessoa.getNome());
+//		psUpdate.setInt(2, pessoa.getCodigo());
+//		psUpdate.setObject(3, pessoa.getId());
+//		
+//		psUpdate.executeUpdate();
+//	}
+//	
+//	private static void deletePessoa(Connection conn, UUID id) throws SQLException {
+//		PreparedStatement psDelete = conn.prepareStatement("Delete from pessoa where id = ?");
+//		psDelete.setObject(1,id);
+//		psDelete.executeUpdate();	
+//	}
+//
+//	private static void deletePessoa(Connection conn, Pessoa pessoa) throws SQLException {
+//	  deletePessoa(conn, pessoa.getId());
+//	}
+//	
+//
+//	private static void createTablePessoa(Connection conn) throws SQLException {
+//      conn.createStatement().execute(
+//    		  "create table if not exists pessoa ("
+//    		  + "id uuid not null primary key,"
+//    		  + "codigo integer not null unique,"
+//    		  + "nome varchar(255) not null"
+//    		  + ")");
+//      conn.createStatement().execute("delete from pessoa");
+//	}
 
 }
---
+++ src/main/java/giovani/bueno/jdbc/repository/AppTempo.java
@@ -0,0 +1,27 @@
+package giovani.bueno.jdbc.repository;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+import javax.swing.JOptionPane;
+
+public class AppTempo {
+
+	public static void main(String[] args) {
+		
+		
+		long inicio  = System.currentTimeMillis();
+		JOptionPane.showMessageDialog(null, "Esperando");
+		System.out.println("Tempo total: " +(System.currentTimeMillis()-inicio));
+		
+		Instant started = Instant.now();
+		JOptionPane.showMessageDialog(null,"Esperando...");
+		Instant finished = Instant.now();
+		
+		System.out.println("Tempo total: " + Duration.between(started,finished).toMillis());
+		
+
+	}
+
+}

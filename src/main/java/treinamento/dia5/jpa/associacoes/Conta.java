a79df4b69749308bec96b684b571fb84b6b09f18
arthur.zavadski <aczavads@gmail.com>
2020-01-10 16:15:05 -0300

    Mapeando associações e herança

---
+++ src/main/java/treinamento/dia5/jpa/associacoes/Conta.java
@@ -0,0 +1,53 @@
+package treinamento.dia5.jpa.associacoes;
+
+import java.time.LocalDate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Conta extends BaseEntity {
+	@Column(nullable = false, unique = true)
+	private Long numero;
+	@Column(nullable = false)
+	private LocalDate abertaEm;
+	
+	@ManyToOne
+	@JoinColumn(name = "correntista_id")
+	private Correntista correntista;
+	
+	public Conta() {
+		super();
+	}
+	public Conta(Correntista correntista, Long numero, LocalDate abertaEm) {
+		super();
+		this.numero = numero;
+		this.abertaEm = abertaEm;
+		this.correntista = correntista;
+		this.correntista.getContas().add(this);
+	}
+	public Correntista getCorrentista() {
+		return correntista;
+	}
+	public LocalDate getAbertaEm() {
+		return abertaEm;
+	}
+	public Long getNumero() {
+		return numero;
+	}
+	public void setAbertaEm(LocalDate abertaEm) {
+		this.abertaEm = abertaEm;
+	}
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+	@Override
+	public String toString() {
+		return "Conta [numero=" + numero + ", abertaEm=" + abertaEm + ", correntista=" + correntista + "]";
+	}
+	
+
+}

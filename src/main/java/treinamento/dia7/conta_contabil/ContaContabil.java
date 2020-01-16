3ce4f298fa5003b95e58bf49ae95e4ae91aca78f
arthur.zavadski <aczavads@gmail.com>
2020-01-16 14:47:31 -0300

    Implementando o put do plano de contas usando DTO e preservando a lista de contas

--- src/main/java/treinamento/dia7/conta_contabil/ContaContabil.java
+++ src/main/java/treinamento/dia7/conta_contabil/ContaContabil.java
@@ -4,13 +4,14 @@ import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import treinamento.dia5.jpa.associacoes.BaseEntity;
+
+
 
 @Entity
 public class ContaContabil extends BaseEntity {

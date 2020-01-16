2ade6819eefaf76d5def4d6fca7ca992d14ca474
gabriel.schiavon <gabriel_schiavon94@hotmail.com>
2020-01-16 11:55:28 -0300

    Adicionando Plano de contas e relacionamentos

--- src/main/java/treinamento/dia7/conta_contabil/ExceptionManager.java
+++ src/main/java/treinamento/dia7/conta_contabil/ExceptionManager.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import schiavon.gabriel.dia7.conta_contabil.BaseController;
 
 @ControllerAdvice(assignableTypes = {BaseController.class})
-public class ControllerExceptionManager {
-	private final Logger logger = LoggerFactory.getLogger(ControllerExceptionManager.class);
+public class ExceptionManager {
+	private final Logger logger = LoggerFactory.getLogger(ExceptionManager.class);
 	
 	@ExceptionHandler({IllegalArgumentException.class})
 	public ResponseEntity<Void> illegalArgument(IllegalArgumentException ex) {
